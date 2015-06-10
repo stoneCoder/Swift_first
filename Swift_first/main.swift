1dda98e8de8febd3f3f6da77951a7759bee95533
stoneCoder <stontzl@gmail.com>
2015-06-10 14:12:44 +0800

    学习

--- Swift_first/main.swift
+++ Swift_first/main.swift
@@ -162,3 +162,209 @@ func makeIncrementer() ->(Int ->Int){
 var increment = makeIncrementer();
 println(increment(7));
 
+//一个函数可以接受其他函数作为参数
+func hasAnyMatches(list:Array<Int>,condition:Int->Bool) ->Bool{
+    for item in list {
+        if condition(item){
+            return true;
+        }
+    }
+    return false;
+}
+
+func lessThanTen(number:Int) -> Bool{
+    return number < 10;
+}
+var numbers = [20,19,7,12];
+println(hasAnyMatches(numbers, lessThanTen));
+
+
+//函数实际是闭包的特殊情况。你可以写一个闭包而无需名字，只需要放在大括号中即可。使用 in 到特定参数和主体的返回值。
+numbers.map({
+    (number:Int)-> Int in
+    let result = 3 * number
+    return result
+})
+
+println(numbers.map({number in 3*number}));
+
+/*创建类*/
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String{
+        return "A shape with \(numberOfSides) sides"
+    }
+}
+var shape = Shape();
+shape.numberOfSides = 7;
+println(shape.simpleDescription());
+
+class NamedShape {
+    var numberOfSides:Int = 0;
+    var name:String;
+    /*结构体*/
+    init(name:String){
+        self.name = name;
+    }
+    
+    func simpleDescription()->String{
+        return "A shape with \(numberOfSides) sides and name is \(self.name)"
+    }
+}
+
+var namedShape = NamedShape(name:"Test");
+namedShape.numberOfSides = 7;
+println(namedShape.simpleDescription());
+
+
+class Square:NamedShape {
+    var sideLength:Double;
+    init(sideLength:Double,name:String){
+        self.sideLength = sideLength;
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area()->Double{
+        return sideLength*sideLength;
+    }
+    /*重载父类的方法*/
+    //子类的方法可以通过标记 override 重载超类中的实现，而没有 override 的会被编译器看作是错误。编译器也会检查那些没有被重载的方法。
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+let test = Square(sideLength: 5.2, name: "my test square");
+println(test.area());
+println(test.simpleDescription());
+
+
+class EquilateralTriangle : NamedShape {
+    var sideLength : Double = 0.0
+    init(sideLength:Double,name:String){
+        self.sideLength = sideLength;
+        super.init(name: name);
+        numberOfSides = 3;
+    }
+    
+    var perimeter:Double{
+        get{
+            return 3.0*sideLength;
+        }
+        
+        set{
+            sideLength = newValue/3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+
+//设置属性的值 调用超类的构造器 改变超类定义的属性的值，添加附加的工作来使用方法、getter、setter也可以在这里
+/*初始化值*/
+let triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle");
+/*调用set方法设置perimeter的值*/
+triangle.perimeter = 9.9
+println(triangle.simpleDescription());
+
+class TriangleAndSquare {
+    //如果你不需要计算属性，但是仍然要提供在设置值之后执行工作，使用 willSet 和 didSet
+    var triangle:EquilateralTriangle{
+        willSet{
+            square.sideLength = newValue.sideLength;
+        }
+    }
+    
+    var square:Square{
+        willSet{
+            triangle.sideLength = newValue.sideLength;
+        }
+    }
+    
+    init(size:Double,name:String){
+        square = Square(sideLength: size, name: name);
+        triangle = EquilateralTriangle(sideLength: size, name: name);
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape");
+println(triangleAndSquare.triangle.sideLength);
+println(triangleAndSquare.square.sideLength);
+triangleAndSquare.square = Square(sideLength: 50.0, name: "larger square")
+println(triangleAndSquare.triangle.sideLength);
+
+//当与可选值一起工作时，你可以写 "?" 到操作符之前类似于方法属性。如果值在"?"之前就已经是 nil ，所有在 "?" 之后的都会自动忽略，而整个表达式是 nil 。另外，可选值是未包装的，所有 "?" 之后的都作为未包装的值。在两种情况中，整个表达式的值是可选值。
+let optionalSquare:Square? = Square(sideLength: 2.5, name: "optional square");
+let sideLength = optionalSquare?.sideLength;
+
+/*枚举*/
+//使用 enum 来创建枚举。有如类和其他命名类型，枚举可以有方法。
+enum Rank:Int{
+    case Ace = 1
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+    
+    func simpleDescription() ->String{
+        switch self{
+            case .Ace: return "ace"
+            case .Jack: return "jack"
+            case .Queen: return "queen"
+            case .King: return "king"
+            default: return String(self.rawValue)
+        }
+    }
+}
+
+let ace = Rank.Jack
+println(ace);
+let aceRawValue = ace.rawValue;
+println(aceRawValue);
+
+
+enum Suit {
+    case Spades, Hearts, Diamonds, Clubs
+    func simpleDescription() -> String {
+        switch self {
+        case .Spades: return "Spades"
+        case .Hearts: return "Hearts"
+        case .Diamonds:return "Diamonds"
+        case .Clubs:  return "Clubs"
+        }
+    }
+}
+let hearts = Suit.Spades //by gashero
+println(hearts.hashValue);
+let heartsDescription = hearts.simpleDescription()
+println(heartsDescription);
+
+//使用 struct 创建结构体。结构体支持多个与类相同的行为，包括方法和构造器。一大重要的区别是代码之间的传递总是用拷贝(值传递)，而类则是传递引用。
+struct Card{
+    var rank:Rank
+    var suit:Suit
+    func simpleDescription() -> String{
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+}
+
+let threeOfSpades = Card(rank: .Three, suit: .Spades);
+println(threeOfSpades.simpleDescription());
+
+
+enum ServerResponse {
+    case Result(String, String)
+    case Error(String)
+}
+let serverResponse:String;
+let success = ServerResponse.Result("6:00 am", "8:09 pm")
+let failure = ServerResponse.Error("Out of cheese.")
+switch success {
+  case let .Result(sunrise, sunset):
+        serverResponse = "Sunrise is at \(sunrise) and sunset is at \(sunset)."
+   case let .Error(error):
+        serverResponse = "Failure... \(error)"
+}
+println(serverResponse);
+
